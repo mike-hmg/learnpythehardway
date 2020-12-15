4771cc7872f836f37281567c285e7ca3fa7cd6c7
Michael Hart <mike@MacBook-Pro.hsd1.nm.comcast.net>
2020-12-15 15:50:08 -0700

    #9

---
+++ exercises/ex9.py
@@ -0,0 +1,16 @@
+## Exercise #9
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# \n denotes new line
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print ("Here are the days: ", days)
+print ("Here are the months", months)
+
+# 3 double quotes lets everything work multiline
+print ("""
+Theres something going on here.
+With the three double quotes
+we'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
+### Remember python3 likes the parenthesis()
